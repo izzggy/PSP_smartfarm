0627d6cc30da908ecfe8f92885db3f9c32705ac5
Boqi Chen <53044040+izzggy@users.noreply.github.com>
2019-11-01 20:39:31 +0800

    Update getimg.py

--- getimg.py
+++ getimg.py
@@ -3,9 +3,11 @@ import cv2
 
 def getfirstframe():
 
-    cap = cv2.VideoCapture('testvideo_pot.mp4')  #返回一个capture对象
-    cap.set(cv2.CAP_PROP_POS_FRAMES,50)  #设置要获取的帧号
-    rtn,frame = cap.read()  #read方法返回一个布尔值和一个视频帧。若帧读取成功，则返回True
+    cap = cv2.VideoCapture('testvideo_pot.mp4')  #return capture
+    cap.set(cv2.CAP_PROP_POS_FRAMES,50)  #frame
+    
+    #return bool, frame
+    rtn,frame = cap.read()  #return True
     
     cv2.imwrite('image1.jpg', frame)
         
